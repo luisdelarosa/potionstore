32fbe6fc2ba4d3db250dd1d86696fe7b79b35514
louie <luis@push.io>
2010-03-25 03:08:39 -0400

    Test that purchasing only creates as many line items for an order as there were in the web form.  This holds true even if the user bought multiple copies of a product.

--- test/functional/store_order_controller_test.rb
+++ test/functional/store_order_controller_test.rb
@@ -52,10 +52,8 @@ class StoreOrderControllerTest < Test::Unit::TestCase
     
     product = Product.create(:price => 24.99)
     assert product
-    
     # This will create one new order with one new line item.
     assert_difference ["Order.count", "LineItem.count"] do
-    
       post :purchase, :order => {
         "payment_type" => "amex",
         "first_name" => "Steve",
@@ -80,6 +78,44 @@ class StoreOrderControllerTest < Test::Unit::TestCase
       "address1" => "123 Infinite Loop"
     end    
     
+    assert_redirected_to :action => :thankyou
+  end
+  
+  def test_purchase_adds_as_many_line_items_as_there_are_line_items_from_form_even_if_user_buys_multiple_copies_of_same_product
+    # Stub out PayPal so we don't call it during testing.
+    Order.any_instance.stubs(:paypal_directcharge).returns(true)
+    
+    product1 = Product.create(:price => 24.99)
+    product2 = Product.create(:price => 14.99)
+    
+    # This will create one new order with one new line item.
+    assert_difference "Order.count", 1 do
+      assert_difference "LineItem.count", 2 do
+        post :purchase, :order => {
+          "payment_type" => "amex",
+          "first_name" => "Steve",
+          "last_name" => "Jobs",
+          "licensee_name" => "Steve Jobs", # This concatenation should be moved to the controller method.
+          "email" => "sjobs@apple.com",
+          "address1" => "123 Infinite Loop",
+          "city" => "Cupertino",
+          "state" => "CA",
+          "zipcode" => "12345",
+          "country" => "US",
+          "cc_number" => "1234567890",
+          "cc_month" => "12",
+          "cc_year" => "2020",
+          "cc_code" => "789"
+      
+        },
+        "items" => {
+          product1.id => 1,
+          product2.id => 3
+        },
+        # It is weird that we have to send address1 again here.
+        "address1" => "123 Infinite Loop"
+      end
+    end    
     
     assert_redirected_to :action => :thankyou
   end
