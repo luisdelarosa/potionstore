96e95c965eb4f7e7b137bc05f1f2979d40647cd7
louie <luis@push.io>
2010-03-25 02:54:10 -0400

    Test the purchase process with credit card.  Note that we stub out the call to PayPal.  Also it is strange that we have to pass in address1 at the top level.

--- test/functional/store_order_controller_test.rb
+++ test/functional/store_order_controller_test.rb
@@ -46,9 +46,31 @@ class StoreOrderControllerTest < Test::Unit::TestCase
     assert_redirected_to :action => :index
   end
 
-  # def test_purchase_should_not_redirect_to_index
-  #   post :purchase, :order => {"foo" => "bar"}, :items => {:foo => :bar}
-  #   # TODO implement
-  # end
+  def test_purchase_should_not_redirect_to_index
+    # Stub out PayPal so we don't call it during testing.
+    Order.any_instance.stubs(:paypal_directcharge).returns(true)
+    
+    post :purchase, :order => {
+      "payment_type" => "amex",
+      "first_name" => "Steve",
+      "last_name" => "Jobs",
+      "licensee_name" => "Steve Jobs", # This concatenation should be moved to the controller method.
+      "email" => "sjobs@apple.com",
+      "address1" => "123 Infinite Loop",
+      "city" => "Cupertino",
+      "state" => "CA",
+      "zipcode" => "12345",
+      "country" => "US",
+      "cc_number" => "1234567890",
+      "cc_month" => "12",
+      "cc_year" => "2020",
+      "cc_code" => "789"
+      
+    }, :items => [],
+    # It is weird that we have to send address1 again here.
+    "address1" => "123 Infinite Loop"
+    
+    assert_redirected_to :action => :thankyou
+  end
 
 end
