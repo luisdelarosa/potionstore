920d3f441cef39506d5497524c5b76fcc5764314
louie <luis@push.io>
2010-03-25 02:48:42 -0400

    Added tests for Order model

--- test/unit/order_test.rb
+++ test/unit/order_test.rb
@@ -18,9 +18,96 @@ class OrderTest < Test::Unit::TestCase
     end
   end
 
-  def test_create_order
-
-
+  def test_create_order_paypal_does_not_validate_anything
+    order = Order.new
+    order.payment_type = "paypal"
+    assert order.save
   end
 
+  def test_create_credit_card_order_validates_name_email_address_and_credit_card_information
+    order = Order.new
+    order.payment_type = "amex"
+    order.first_name = "Steve"
+    order.last_name = "Jobs"
+    
+    order.email = "sjobs@apple.com"
+    
+    order.address1 = "123 Infinite Loop"
+    order.city = "Cupertino"
+    order.state = "CA"
+    order.zipcode = "12345"
+    order.country = "US"
+    
+    order.cc_number = "1234567890"
+    order.cc_month = "12"
+    order.cc_year = "2020"
+    order.cc_code = "789"
+    assert order.save
+  end
+  
+  def test_create_credit_order_checks_first_name
+    order = Order.new
+    order.payment_type = "amex"
+    # order.first_name = "Steve"
+    order.last_name = "Jobs"
+    
+    order.email = "sjobs@apple.com"
+    
+    order.address1 = "123 Infinite Loop"
+    order.city = "Cupertino"
+    order.state = "CA"
+    order.zipcode = "12345"
+    order.country = "US"
+    
+    order.cc_number = "1234567890"
+    order.cc_month = "12"
+    order.cc_year = "2020"
+    order.cc_code = "789"
+    assert_equal false, order.save
+  end
+  
+  def test_create_credit_order_checks_last_name
+    order = Order.new
+    order.payment_type = "amex"
+    order.first_name = "Steve"
+    # order.last_name = "Jobs"
+    
+    order.email = "sjobs@apple.com"
+    
+    order.address1 = "123 Infinite Loop"
+    order.city = "Cupertino"
+    order.state = "CA"
+    order.zipcode = "12345"
+    order.country = "US"
+    
+    order.cc_number = "1234567890"
+    order.cc_month = "12"
+    order.cc_year = "2020"
+    order.cc_code = "789"
+    assert_equal false, order.save
+  end
+
+  def test_create_credit_order_checks_email
+    order = Order.new
+    order.payment_type = "amex"
+    order.first_name = "Steve"
+    order.last_name = "Jobs"
+    
+    # order.email = "sjobs@apple.com"
+    
+    order.address1 = "123 Infinite Loop"
+    order.city = "Cupertino"
+    order.state = "CA"
+    order.zipcode = "12345"
+    order.country = "US"
+    
+    order.cc_number = "1234567890"
+    order.cc_month = "12"
+    order.cc_year = "2020"
+    order.cc_code = "789"
+    assert_equal false, order.save
+  end
+  
+  # TODO write tests for checking address and credit card info
+  
 end
